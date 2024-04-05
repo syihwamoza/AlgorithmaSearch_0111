1b6ab3ade1d9c21ffc2a4fee628c38f4c065488b
syihwamoza <mozakastella26@gmail.com>
2024-04-05 10:08:37 +0700

    memasukkan procedur

--- algo5April/algo5April.cpp
+++ algo5April/algo5April.cpp
@@ -8,4 +8,13 @@ int i;
 void input()
 {
 	while (true)
+	{
+		cout << "Enter the number of element in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements. \n\n ";
+
+	}
 }
