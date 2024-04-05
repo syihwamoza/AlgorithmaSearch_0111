be35d9f4feb3244a3f50942ed398d6ba9bfecbaf
syihwamoza <mozakastella26@gmail.com>
2024-04-05 11:02:50 +0700

    memasukkan void display

--- algo5April/algo5April.cpp
+++ algo5April/algo5April.cpp
@@ -51,7 +51,13 @@ void LinearSearch()
 		}
 
 		if (i == n)
+			cout << "\n" << item << "not found in the array\n";
+		cout << "\nNumber of comparisons:" << ctr << endl;
 
+		cout << "\nContinue search (y/n):";
+		cin >> ch;
 
-	}
+	} while ((ch == 'y') || (ch == 'Y'));
 }
+
+void display ()
