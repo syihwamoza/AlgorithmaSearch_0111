a64c2eed71cce3bb1db10c00d79d250ef365bdc3
syihwamoza <mozakastella26@gmail.com>
2024-04-05 10:55:30 +0700

    memasukkan pengecekan jika element sdh ditemukan

--- algo5April/algo5April.cpp
+++ algo5April/algo5April.cpp
@@ -41,7 +41,13 @@ void LinearSearch()
 		i = 0;
 		while (i < n)
 		{
+			ctr++;
+			if (arr[i] == item)
+			{
+				cout << "\n" << item << "found at position" << (i + 1) << endl;
+				
 
+			}
 		}
 
 
