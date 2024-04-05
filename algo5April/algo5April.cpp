c67698f219553141b7eccb225a52b7f46fd22f6c
syihwamoza <mozakastella26@gmail.com>
2024-04-05 10:14:51 +0700

    memasukkan void LinearSearch

--- algo5April/algo5April.cpp
+++ algo5April/algo5April.cpp
@@ -20,5 +20,13 @@ void input()
 	cout << "\n Enter array elements \n";
 	cout << "----------------------------\n";
 	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
+
+void LinearSearch()
+{
 
 }
